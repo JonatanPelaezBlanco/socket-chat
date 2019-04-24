f8b6bf81afd6a86eacfb4f7a0f5fdd6026869d12
Jonatan <jpelaezblanco@hotmail.com>
2019-04-24 17:19:32 +0200

    Chat finalizado v1

--- public/js/socket-chat.js
+++ public/js/socket-chat.js
@@ -4,51 +4,47 @@ var params = new URLSearchParams(window.location.search);
 
 if (!params.has('nombre') || !params.has('sala')) {
     window.location = 'index.html';
-    throw new Error('El nombre y la sala son necesarios');
+    throw new Error('El nombre y sala son necesarios');
 }
 
 var usuario = {
     nombre: params.get('nombre'),
     sala: params.get('sala')
-}
+};
+
+
 
 socket.on('connect', function() {
-
     console.log('Conectado al servidor');
 
     socket.emit('entrarChat', usuario, function(resp) {
-        console.log('Usuarios conectados: ', resp);
+        renderizarUsuarios(resp);
     });
+
 });
 
 // escuchar
 socket.on('disconnect', function() {
 
     console.log('Perdimos conexión con el servidor');
+
 });
-
-
-// // Enviar información
-// socket.emit('enviarMensaje', {
-//     usuario: 'Fernando',
-//     mensaje: 'Hola Mundo'
-// }, function(resp) {
-//     console.log('respuesta server: ', resp);
-// });
 
 // Escuchar información
 socket.on('crearMensaje', function(mensaje) {
-
-    console.log('Servidor:', mensaje);
+    renderizarMensajes(mensaje, false);
+    scrollBottom();
 });
 
-//Cuando un usuario entre o sale del chat
-socket.on('listaPersonas', function(usuarios) {
-
-    console.log(usuarios);
+// Escuchar cambios de usuarios
+// cuando un usuario entra o sale del chat
+socket.on('listaPersona', function(personas) {
+    renderizarUsuarios(personas);
 });
 
-//Mensajes privados
+// Mensajes privados
 socket.on('mensajePrivado', function(mensaje) {
-    console.log('Mensaje Privado: ', mensaje);
-})
+
+    console.log('Mensaje Privado:', mensaje);
+
+});
